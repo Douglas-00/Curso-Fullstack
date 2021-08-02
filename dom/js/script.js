6c32b1f671a57dc7c1f3bf60861f64f5737fe517
Douglas-00 <alves.douglas547@gmail.com>
2021-08-02 15:54:07 -0300

    subindo

--- dom/js/script.js
+++ dom/js/script.js
@@ -1,4 +1,4 @@
-/*function log() {
+function log(){
     console.log(this)
 }
 HTMLCollection.prototype.log = log
@@ -7,6 +7,5 @@ NodeList.prototype.log = log
 Node.prototype.log = log
 
 const lista = document.querySelector('div ul')
-lista.parentNode.log()*/
-
-document.querySelectorAll('div ul li').log()
+lista.parentNode.log()
+lista.childNodes.log()
