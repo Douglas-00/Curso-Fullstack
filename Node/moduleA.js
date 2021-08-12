554d65fe8bac32d4ff10f61e3657cf21e8e0f93a
Douglas-00 <alves.douglas547@gmail.com>
2021-08-12 16:02:07 -0300

    ...

---
+++ Node/moduleA.js
@@ -0,0 +1,6 @@
+//Em Node um arquivo significa um modulo
+
+this.ola = 'Fala pessoal'
+
+exports.bemvindo = 'Bem vindo ao Node'
+module.exports.atelogo = 'Até a Próximo Exemplo'
---
+++ Node/modulecliente.js
@@ -0,0 +1,12 @@
+//Importando os modulos criados com require
+
+
+const moduloA = require('./moduleA')
+
+const moduloB = require('./moduleB')
+
+console.log(moduloA.ola)
+
+console.log(moduloA.bemvindo)
+
+console.log(moduloA.atelogo)
