118a58eb39232c23efaf24416d3a438a6e898546
Douglas-00 <alves.douglas547@gmail.com>
2021-08-25 15:57:55 -0300

    ...

--- Nextjs/forof.js
+++ Nextjs/forof.js
@@ -2,3 +2,34 @@
 for (let letra of "Cod3r"){
     console.log(letra)
 }
+
+const assuntosecma = ['Map', 'Set', 'Promise']
+
+for (let i in assuntosecma){
+    console.log(i)
+}
+
+for (let assunto of assuntosecma){
+    console.log(assunto)
+}
+
+const assuntosMap = new Map([
+    ['Map', {abordado: true}],
+    ['Set', {abordado:true}],
+    ['Promise', {abordado:false}]
+])
+
+for(let assunto of assuntosMap){
+    console.log(assunto)
+}
+//Percorrendo as chaves 
+
+for (let chave of assuntosMap.keys()){
+    console.log(chave)
+}
+
+//Percorrendo os valores
+
+for(let valor of assuntosMap.values()){
+    console.log(valor)
+}
